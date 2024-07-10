c2923b62f5651bfdebbadb7e1f02f786458ec3ee
Ragavi P <156108707+Ragavi04P@users.noreply.github.com>
2024-07-10 21:30:14 +0530

    Update first_instance.tf

--- first_instance.tf
+++ first_instance.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "intro" {
   instance_type          = "t2.micro"
   availability_zone      = "us-east-2a"
   key_name               = "key-aws"
-  vpc_security_group_ids = ["sg-0e2598a67bf63050a"]
+  vpc_security_group_ids = ["sg-0e2598a67bf63090a"]
   tags = {
     Name    = "sample-Instance"
     Project = "Terraform"
